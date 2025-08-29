5daf00026dad73ca15849f5b77fd3018f0c65a7f
AbeTakaki <abetakaki0101@gmail.com>
2025-08-29 09:47:45 +0900

    加算クイズ作成

---
+++ arithmetic-oprerations-quiz/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    // 加算クイズ
+    println!("1 + 1 = ??");
+    println!("?? の値を入力してください：");
+    // ユーザからの入力を保持する変数を用意
+    let mut ans_input = String::new();
+    // 標準入力から1行取得し、 ans_input に代入
+    std::io::stdin().read_line(&mut ans_input).unwrap();
+
+    // ans_input から trim()で改行を取り除き、parse()で整数(u32)型に変換
+    let ans_input = ans_input.trim().parse::<u32>().unwrap();
+    dbg!(ans_input);
+
+    if dbg!(ans_input == 1 + 1) {
+        println!("正解です！");
+    } else {
+        println!("不正解です");
+    }
+
+    // 減算クイズ
+    println!("1 + 1 = ??");
+    println!("?? の値を入力してください：");
+    // ユーザからの入力を保持する変数を用意
+    let mut ans_input = String::new();
+    // 標準入力から1行取得し、 ans_input に代入
+    std::io::stdin().read_line(&mut ans_input).unwrap();
+
+    // ans_input から trim()で改行を取り除き、parse()で整数(u32)型に変換
+    let ans_input = ans_input.trim().parse::<u32>().unwrap();
+    dbg!(ans_input);
+
+    if dbg!(ans_input == 1 + 1) {
+        println!("正解です！");
+    } else {
+        println!("不正解です");
+    }
+}
