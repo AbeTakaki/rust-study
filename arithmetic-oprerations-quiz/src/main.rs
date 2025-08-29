632032e2f23d2458cd143c04bd19c396f022dd5a
AbeTakaki <abetakaki0101@gmail.com>
2025-08-29 15:12:26 +0900

    3問正解するまでループするように修正

--- arithmetic-oprerations-quiz/src/main.rs
+++ arithmetic-oprerations-quiz/src/main.rs
@@ -1,47 +1,63 @@
 use rand::Rng;
 
 fn main() {
-    // 加算クイズ
-    // ランダムな数字を生成
-    let op1 = rand::rng().random_range(0..100);
-    let op2 = rand::rng().random_range(0..100);
+    // 正解数を数える変数を追加
+    let mut num_of_correct = 0;
+    // 正解数が3以下の時は繰り返す
+    while num_of_correct < 3 {
+        // 加算クイズ
+        // ランダムな数字を生成
+        let op1 = rand::rng().random_range(0..100);
+        let op2 = rand::rng().random_range(0..100);
 
-    println!("{} + {} = ??", op1, op2);
-    println!("?? の値を入力してください：");
-    // ユーザからの入力を保持する変数を用意
-    let mut ans_input = String::new();
-    // 標準入力から1行取得し、 ans_input に代入
-    std::io::stdin().read_line(&mut ans_input).unwrap();
+        println!("{} + {} = ??", op1, op2);
+        println!("?? の値を入力してください：");
+        // ユーザからの入力を保持する変数を用意
+        let mut ans_input = String::new();
+        // 標準入力から1行取得し、 ans_input に代入
+        std::io::stdin().read_line(&mut ans_input).unwrap();
 
-    // ans_input から trim()で改行を取り除き、parse()で整数(i32)型に変換
-    let ans_input = ans_input.trim().parse::<i32>().unwrap();
-    dbg!(ans_input);
+        // ans_input から trim()で改行を取り除き、parse()で整数(i32)型に変換
+        let ans_input = ans_input.trim().parse::<i32>().unwrap();
+        dbg!(ans_input);
 
-    if dbg!(ans_input == op1 + op2) {
-        println!("正解です！");
-    } else {
-        println!("不正解です");
+        if ans_input == op1 + op2 {
+            println!("正解です！");
+            // 正解したら正解数を1増やす
+            num_of_correct += 1;
+            // 3問正解したらループを抜ける
+            if num_of_correct == 3 {
+                break;
+            };
+        } else {
+            println!("不正解です");
+        }
+
+        // 減算クイズ
+        // ランダムな数字を生成
+        let op1 = rand::rng().random_range(0..100);
+        let op2 = rand::rng().random_range(0..100);
+
+        println!("{} - {} = ??", op1, op2);
+        println!("?? の値を入力してください：");
+        // ユーザからの入力を保持する変数を用意
+        let mut ans_input = String::new();
+        // 標準入力から1行取得し、 ans_input に代入
+        std::io::stdin().read_line(&mut ans_input).unwrap();
+
+        // ans_input から trim()で改行を取り除き、parse()で整数(i32)型に変換
+        let ans_input = ans_input.trim().parse::<i32>().unwrap();
+        if ans_input == op1 - op2 {
+            println!("正解です！");
+            // 正解したら正解数を1増やす
+            num_of_correct += 1;
+            // 3問正解したらループを抜ける
+            if num_of_correct == 3 {
+                break;
+            };
+        } else {
+            println!("不正解です");
+        }
     }
-
-    // 減算クイズ
-    // ランダムな数字を生成
-    let op1 = rand::rng().random_range(0..100);
-    let op2 = rand::rng().random_range(0..100);
-
-    println!("{} - {} = ??", op1, op2);
-    println!("?? の値を入力してください：");
-    // ユーザからの入力を保持する変数を用意
-    let mut ans_input = String::new();
-    // 標準入力から1行取得し、 ans_input に代入
-    std::io::stdin().read_line(&mut ans_input).unwrap();
-
-    // ans_input から trim()で改行を取り除き、parse()で整数(i32)型に変換
-    let ans_input = ans_input.trim().parse::<i32>().unwrap();
-    dbg!(ans_input);
-
-    if dbg!(ans_input == op1 - op2) {
-        println!("正解です！");
-    } else {
-        println!("不正解です");
-    }
+    println!("クリア");
 }
